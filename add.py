fe5e42b6c976926c60a7a28fc318049563413833
Alexandr <apower3@bk.ru>
2021-09-21 14:58:30 +0300

    Sites

--- add.py
+++ add.py
@@ -1,4 +1,32 @@
-a = 3
-b = 5
-print(a + b)
-print(a - b - a)
+from pprint import pprint
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+#  ((x1-x2)**2 + (y1-y2)**2))**.5
+distances = dict()
+
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_london = ((moscow[0] - london[0])**2 + (moscow[1] - london[1])**2)**.5
+moscow_paris = ((moscow[0] - paris[0])**2 + (moscow[1] - paris[1])**2)**.5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+london_paris = ((london[0] - paris[0])**2 + (london[1] - paris[1])**2)**.5
+
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
+
+pprint(distances)
+print(distances['Moscow']['London'])
